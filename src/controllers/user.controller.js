4f9c7be16475e2de9018dd7b5d323b01de697313
Mitesh-Amin01 <aminmiteshkumar.1484@gmail.com>
2025-06-17 02:45:07 +0530

    User SignUp Comtroller Code added!!!

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,25 @@
+import { ApiError } from '../utils/ApiError.js'
 import { asyncHandler } from '../utils/asyncHandler.js'
+import { User } from '../models/user.models.js'
+import { ApiResponse } from '../utils/ApiResponse.js'
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ massage: "ok" })
+    const { username, email, phone, fullname, password } = req.body
+
+    if ([fullname, email, username, phone, password].some((field) => field.trim() === "")) {
+        throw new ApiError(400, "All Fields Are Required")
+    }
+    const existedUser = await User.findOne({ $or: [{ username }, { email }] })
+    console.log("DEBUGE:- ",existedUser)
+    if(existedUser){
+        throw new ApiError(409, "User With Email or Username Already Exists!")
+    }
+    const user = await User.create({username,fullname,email,phone,password})
+    const createdUser = await User.findById(user._id)
+    if(!createdUser){
+        throw new ApiError(500,"Something Went Wrong While Registering The User")
+    }
+    return res.status(201).json(new ApiResponse(200,createdUser,"User Registered Successfully"))
 })
 
-export {registerUser}
+export { registerUser }
