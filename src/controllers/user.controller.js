04da601fe17f99904f8c89f028e7cc750d09a317
Mitesh-Amin01 <aminmiteshkumar.1484@gmail.com>
2025-06-19 19:37:26 +0530

    Login/Logout Route And Controller Build!

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,19 @@ import { User } from '../models/user.models.js'
 import { ApiResponse } from '../utils/ApiResponse.js'
 import { uploadOnCloudinary } from '../utils/cloudnary.js'
 
+const generateTokens = async (userId) => {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+        return { accessToken, refreshToken }
+    } catch (error) {
+        throw new ApiError(500, "Something Went Wrong While generating refresh and access token")
+    }
+}
+
 const registerUser = asyncHandler(async (req, res) => {
     const { username, email, phone, fullname, password } = req.body
 
@@ -15,11 +28,11 @@ const registerUser = asyncHandler(async (req, res) => {
     if (existedUser) {
         throw new ApiError(409, "User With Email or Username Already Exists!")
     }
-    console.log("Check: ",req.file.path)
+    console.log("Check: ", req.file.path)
     const profileLocalPath = req.file?.path
     if (!profileLocalPath) console.log("Not Found Profile Image Parh! ", profileLocalPath);
     const profileImage = await uploadOnCloudinary(profileLocalPath)
-    console.log("Check:- ",profileLocalPath)
+    console.log("Check:- ", profileLocalPath)
     const user = await User.create({
         username,
         fullname,
@@ -35,4 +48,66 @@ const registerUser = asyncHandler(async (req, res) => {
     return res.status(201).json(new ApiResponse(200, createdUser, "User Registered Successfully"))
 })
 
-export { registerUser }
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, phoneNumber, password } = req.body
+    if (!email || !phoneNumber) {
+        throw ApiError(400, "Email/PhoneNumber And Password Required!")
+    }
+    const user = User.findOne({ $or: [{ email }, { phoneNumber }] })
+
+    if (!user) {
+        throw ApiError(404, "User Dose Not Exist!")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if (!isPasswordValid) {
+        throw ApiError(401, "Invalid User Credentials!")
+    }
+
+    const { accessToken, refreshToken } = await generateTokens(user._id)
+    const loggedInUser = User.findById(user._id).select("-refreshToken -password")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    user: accessToken, refreshToken, loggedInUser
+                },
+                "User Logged In Successfully"
+            )
+        )
+})
+
+const logoutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+    return res
+        .status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .josn(new ApiResponse(200, {}, "User Logged Out"))
+})
+
+export { registerUser, loginUser, logoutUser }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express'
-import { registerUser } from '../controllers/user.controller.js'
+import { loginUser, logoutUser, registerUser } from '../controllers/user.controller.js'
 import { upload } from '../middleware/multer.middleware.js'
+import { varifayJWT } from '../middleware/auth.middleware.js'
 const router = Router()
 
 router.route("/register").post(
     upload.single("avatar")
     ,registerUser
 )
+router.route("/login").post(loginUser)
+router.route("/logout").post(varifayJWT, logoutUser)
 export default router
